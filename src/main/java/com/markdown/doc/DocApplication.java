d21d4fe8ae2fc635dafa656c7a9764ee225e5e84
Pulsara <pulsarasandeepa@gmail.com>
2021-05-23 12:58:53 +0530

    begin

---
+++ src/main/java/com/markdown/doc/DocApplication.java
@@ -0,0 +1,15 @@
+package com.markdown.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableMongoAuditing
+public class DocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocApplication.class, args);
+    }
+
+}
